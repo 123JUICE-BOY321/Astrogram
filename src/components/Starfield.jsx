035a922901ab025df0e8862c6f1ee288cecbddda
Tanishq Shreeyas <shreeyas123456@gmail.com>
2025-11-30 11:20:26 +0530

    Skeleton 1

---
+++ src/components/Starfield.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Starfield = () => {
+  return (
+    <div>Starfield</div>
+  )
+}
+
+export default Starfield
---
+++ src/pages/Stellarium.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Stellarium = () => {
+  return (
+    <div>Stellarium</div>
+  )
+}
+
+export default Stellarium
